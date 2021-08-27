a615ded8665283936c108206f94665d85c407566
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2021-08-26 22:05:17 -0300

    added BusinessAccount class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
+import entities.BusinessAccount;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BusinessAccount account = new BusinessAccount();
+		
 	}
 
 }
